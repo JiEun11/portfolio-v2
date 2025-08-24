73741b272272754ee08d609d59ff1687a3650639
adalyn.kim <adalyn.kim@goorm.io>
2025-08-24 23:09:38 +0900

    fix: ArrowUp 컴포넌트 scss module화

--- client/src/components/ArrowUp/ArrowUp.tsx
+++ client/src/components/ArrowUp/ArrowUp.tsx
@@ -1,13 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import styles from "./ArrowUp.module.scss";
 
 const ArrowUp: React.FC = () => {
+    const [isVisible, setIsVisible] = useState(false);
+
+    const handleScroll = () => {
+        if (window.scrollY > window.innerHeight / 2) {
+            setIsVisible(true);
+        } else {
+            setIsVisible(false);
+        }
+    };
+
     const handleClick = () => {
         window.scrollTo({ top: 0, behavior: "smooth" });
     };
 
+    useEffect(() => {
+        window.addEventListener("scroll", handleScroll);
+
+        // 컴포넌트 언마운트 시 스크롤 이벤트 제거
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, []);
+
     return (
         <button
-            className="arrow-up"
+            className={`${styles.arrowUp} ${isVisible ? styles.visible : ''}`}
             onClick={handleClick}
             aria-label="맨 위로 이동"
         >
